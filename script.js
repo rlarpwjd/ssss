32c03eb90478d1da0142f80a1882cc9f662b36e6
rlarpwjd <aodnvudqjagksrPwjd@gmail.com>
2026-07-30 16:25:50 +0900

    Remove lane creation logic from script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const editor = document.getElementById("editor");
 
-for(let i = 0; i < 20; i++){
-    const lane = document.createElement("div");
-    lane.className = "lane";
-    editor.appendChild(lane);
-}
